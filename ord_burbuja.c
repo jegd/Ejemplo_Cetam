9950af0034d90ffaa1f6831c2945d84231625029
Jesus Gonzales <jesus.e_97@hotmail.com>
2022-05-16 22:36:59 -0500

    Version ordenado

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -9,13 +9,26 @@ int main()
     int ord_vector[size];
     srand(time(NULL));
     printf("Generacion de numeros aleatorios");
-    int i= 0;
+    int i= 0, j= 0;
+    int aux;
     for(i=0; i<size; i++)
     {
         ord_vector[i] = 100 + rand()%(101);
         printf("\n");
         printf("%d", ord_vector[i]);
     }
+for(i = 0; i< size ; i++)
+{
+for(j = 0; j < size -i -1; j++)
+{
+    if(ord_vector[j]>ord_vector[j+1])
+    {
+        aux = ord_vector[j];
+        ord_vector[j] = ord_vector[j+1];
+        ord_vector[j+1] = aux;
+    }
+}
 
+}
     
 }
