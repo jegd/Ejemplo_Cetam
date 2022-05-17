aaf399dbdaab7a07d752a4c60ae96a32c5f954fd
Jesus Gonzales <jesus.e_97@hotmail.com>
2022-05-16 22:19:26 -0500

    Generacion de numeros aleatorios

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define size 10
+
+int main()
+{
+    int ord_vector[size];
+    srand(time(NULL));
+    printf("Generacion de numeros aleatorios");
+    int i= 0;
+    for(i=0; i<size; i++)
+    {
+        ord_vector[i] = 100 + rand()%(101);
+        printf("\n");
+        printf("%d", ord_vector[i]);
+    }
+
+    
+}
